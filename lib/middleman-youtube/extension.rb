9871e461677994a2077edbebff7d40acea674159
yterajima <terra@e2esound.com>
2015-02-24 19:07:45 +0900

    Fix converting url

--- lib/middleman-youtube/extension.rb
+++ lib/middleman-youtube/extension.rb
@@ -5,6 +5,19 @@ module Middleman
 
       def initialize(app, options_hash = {}, &block)
         super
+        extension = self
+        app.before_render do |body|
+          extension.convert(body)
+        end
+      end
+
+      def convert(body)
+        regex = %r{https://www.youtube.com/watch\?v=(.*?)(\s+)?$}
+        body.gsub(regex) { iframe($1) }
+      end
+
+      def iframe(path)
+        '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + path + '" frameborder="0" allowfullscreen="true"></iframe>'
       end
     end
   end
