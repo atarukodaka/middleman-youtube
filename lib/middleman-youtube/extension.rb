af0bdfae65042c983fe31e60675a55f0f15b96d2
yterajima <terra@e2esound.com>
2015-02-25 00:33:06 +0900

    Complete convert.feature

--- lib/middleman-youtube/extension.rb
+++ lib/middleman-youtube/extension.rb
@@ -17,7 +17,15 @@ module Middleman
       end
 
       def iframe(path)
-        '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + path + '" frameborder="0" allowfullscreen="true"></iframe>'
+        '<iframe width="' + width.to_s + '" height="' + height.to_s + '" src="https://www.youtube.com/embed/' + path + '" frameborder="0" allowfullscreen="true"></iframe>'
+      end
+
+      def width
+        options[:width]
+      end
+
+      def height
+        (options[:width] * (315.0 / 560.0)).round
       end
     end
   end
